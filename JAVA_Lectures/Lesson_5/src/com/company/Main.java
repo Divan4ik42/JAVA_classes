d008d0b1e20d27d50e4405dfeba542500a55af3f
java01 <divan4ik42@gmail.com>
2020-07-23 18:59:53 +0300

    Lesson_5 ADDED
    start

--- JAVA_Lectures/Lesson_5/src/com/company/Main.java
+++ JAVA_Lectures/Lesson_5/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        // загружаем файл в строку
+        String text = new String(Files.readAllBytes(Paths.get("C:/Users/Java01/Documents/divan/GITHUB/JAVA_Lectures/Lesson_5/Harry Potter and the Sorcerer.txt")));
+        System.out.println(text);
     }
 }
+
+       
