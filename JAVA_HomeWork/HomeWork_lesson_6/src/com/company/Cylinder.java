b48bda6cbfaf5cb409ff3fb9428ec090e0c1439c
Divan4ik <Divan4ik42@gmail.com>
2020-07-30 14:22:51 +0300

    HomeWork_Lesson_6 DONE 42

--- JAVA_HomeWork/HomeWork_lesson_6/src/com/company/Cylinder.java
+++ JAVA_HomeWork/HomeWork_lesson_6/src/com/company/Cylinder.java
@@ -1,5 +1,48 @@
 package com.company;
 
 public class Cylinder {
-    
+    private int height;
+    private int radius;
+
+    public Cylinder(int height, int radius) {
+        this.height = height;
+        this.radius = radius;
+    }
+
+    double getPerimeter() {
+        return (2 * Math.PI * radius);
+        }
+    double getSideArea(){
+        return getPerimeter() * height;
+    }
+    double getCircleArea (){
+        return Math.PI * radius * radius;
+    }
+    double geTotalCylinderArea (){
+        return getSideArea() + (2 * getCircleArea());
+    }
+
+    @Override
+    public String toString() {
+        return "--------------------------------------------" + "\n" +
+                "Cylinder { " +
+                "height = " + height +
+                ", radius = " + radius + " }" + "\n" +
+                " "  + "\n" +
+                "   CALCULATIONS ARE :       " + "\n" +
+                " "  + "\n" +
+                "Circle perimeter = " + getPerimeter() + "\n" +
+                "Circle area = " + getCircleArea() + "\n" +
+                "Cylinder side area = " + getSideArea() + "\n" +
+                "Cylinder TOTAL area = " + geTotalCylinderArea() + "\n" +
+                 "--------------------------------------------";
+
+    }
+
+
+    // периметр perimetr = 2PI*R
+    // sideArea = rerimetr(2PI*R) * height
+    // circleArea = PI*R*R
+    // totalCilinderArea = 2pi*R*heught + 2*pi*R*R
+
 }
--- JAVA_HomeWork/HomeWork_lesson_6/src/com/company/Main.java
+++ JAVA_HomeWork/HomeWork_lesson_6/src/com/company/Main.java
@@ -3,7 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// CILINDER
-        
+
+        Cylinder c1 = new Cylinder(2,5);
+        double PerimeterC1 = c1.getPerimeter();
+        double sideAreaC1 = c1.getSideArea();
+        double circleAreaC1 = c1.getCircleArea();
+        double totalCylinderAreaC1 = c1.geTotalCylinderArea();
+
+        System.out.println(c1.toString());
+
+        Cylinder c2 = new Cylinder(4,2);
+        double PerimeterC2 = c2.getPerimeter();
+        double sideAreaC2 = c2.getSideArea();
+        double circleAreaC2 = c2.getCircleArea();
+        double totalCylinderAreaC2 = c2.geTotalCylinderArea();
+
+        System.out.println(c2.toString());
+
+        Cylinder c3 = new Cylinder(24,6);
+        double PerimeterC3 = c3.getPerimeter();
+        double sideAreaC3 = c3.getSideArea();
+        double circleAreaC3 = c3.getCircleArea();
+        double totalCylinderAreaC3 = c3.geTotalCylinderArea();
+
+        System.out.println(c3.toString());
+
+
     }
 }
